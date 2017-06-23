da4c47d37ce5027f92f890e7bcf70a1e6a266391
Tim Rühsen <tim.ruehsen@gmx.de>
2017-06-23 16:03:30 +0200

    Add initial OSS-Fuzz testing

---
+++ fuzz/README.md
@@ -0,0 +1,65 @@
+# Fuzzers
+
+These are fuzzers designed for use with `libFuzzer` or `afl`. They can
+be used to run on Google's OSS-Fuzz (https://github.com/google/oss-fuzz/).
+
+The convention used here is that the initial values for each parser fuzzer
+are taken from the $NAME.in directory.
+
+Crash reproducers from OSS-Fuzz are put into $NAME.repro directory for
+regression testing with top dir 'make check' or 'make check-valgrind'.
+
+The ./configure runs below are for libidn.
+
+
+# Running a fuzzer using clang
+
+Use the following commands on top dir:
+```
+export CC=clang-5.0
+export CXX=clang++-5.0
+export CFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize-coverage=trace-pc-guard,trace-cmp"
+export CXXFLAGS="-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -fsanitize=address -fsanitize-address-use-after-scope -fsanitize-coverage=trace-pc-guard,trace-cmp -stdlib=libc++"
+ASAN_OPTIONS=detect_leaks=0 ./configure --enable-static --disable-gtk-doc-html
+make clean
+make -j$(nproc)
+cd fuzz
+
+# build and run libpsl_fuzzer
+./run-clang.sh libidn_toascii_fuzzer
+```
+
+
+# Running a fuzzer using AFL
+
+Use the following commands on top dir:
+
+```
+$ CC=afl-clang-fast ./configure --disable-gtk-doc-html
+$ make -j$(nproc) clean all
+$ cd fuzz
+$ ./run-afl.sh libidn_fuzzer
+```
+
+# Fuzz code coverage using the corpus directories *.in/
+
+Code coverage reports currently work best with gcc+lcov+genhtml.
+
+In the top directory:
+```
+CC=gcc CFLAGS="-O0 -g" ./configure --disable-gtk-doc-html
+make coverage
+xdg-open doc/coverage/index.html
+```
+
+Each fuzzer target has it's own functions to cover, e.g.
+`libidn_toascii_fuzzer` covers idna_to_ascii_* functions.
+
+To work on corpora for better coverage, `cd fuzz` and use e.g.
+`./view-coverage.sh libidn_toascii_fuzzer`.
+
+
+# Enhancing the testsuite for issues found
+
+Each reproducer file should be dropped into the appropriate *.repro/
+directory.
---
+++ fuzz/libidn_toascii_fuzzer.c
@@ -0,0 +1,84 @@
+/*
+ * Copyright(c) 2017 Tim Ruehsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This file is part of libidn.
+ */
+
+#include <config.h>
+
+#include <assert.h> // assert
+#include <stdint.h> // uint8_t
+#include <stdlib.h> // malloc, free
+#include <string.h> // memcpy
+
+#if defined(WITH_LIBICU)
+#include <unicode/uclean.h>
+#endif
+
+#include "idna.h"
+#include "idn-free.h"
+#include "fuzzer.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+	char *domain = (char *) malloc(size + 1), *res;
+	char *asc = (char *) malloc(64);
+	uint32_t *ucs4label, *ucs4label_x;
+	char *out;
+	size_t items_written;
+	int rc;
+
+	assert(domain != NULL);
+	assert(asc != NULL);
+
+	if ((size & 3) == 0) {
+		uint32_t *data0 = malloc((size + 1) * 4);
+
+		idna_to_ascii_4i((uint32_t *)data, size / 4, asc, 0);
+		idna_to_ascii_4i((uint32_t *)data, size / 4, asc, IDNA_ALLOW_UNASSIGNED|IDNA_USE_STD3_ASCII_RULES);
+
+		memcpy(data0, data, size);
+		data0[size] = 0;
+		if (idna_to_ascii_4z(data0, &out, 0) == IDNA_SUCCESS)
+			idn_free(out);
+		if (idna_to_ascii_4z(data0, &out, IDNA_ALLOW_UNASSIGNED|IDNA_USE_STD3_ASCII_RULES) == IDNA_SUCCESS)
+			idn_free(out);
+		free(data0);
+	}
+
+	// 0 terminate
+	memcpy(domain, data, size);
+	domain[size] = 0;
+
+	if (idna_to_ascii_8z(domain, &out, 0) == IDNA_SUCCESS)
+		idn_free(out);
+	if (idna_to_ascii_8z(domain, &out, IDNA_ALLOW_UNASSIGNED|IDNA_USE_STD3_ASCII_RULES) == IDNA_SUCCESS)
+		idn_free(out);
+	if (idna_to_ascii_lz(domain, &out, 0) == IDNA_SUCCESS)
+		idn_free(out);
+	if (idna_to_ascii_lz(domain, &out, IDNA_ALLOW_UNASSIGNED|IDNA_USE_STD3_ASCII_RULES) == IDNA_SUCCESS)
+		idn_free(out);
+
+	free(asc);
+	free(domain);
+
+	return 0;
+}
