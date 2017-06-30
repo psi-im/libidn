39d98432726a7f40e9a7b58e517b07f7bbbf5b2a
Tim Rühsen <tim.ruehsen@gmx.de>
2017-06-30 11:54:23 +0200

    Fix NULL ptr access in stringprep_ucs4_nfkc_normalize(), foudn by fuzzing

--- lib/nfkc.c
+++ lib/nfkc.c
@@ -1099,6 +1099,7 @@ stringprep_utf8_nfkc_normalize (const char *str, ssize_t len)
   return g_utf8_normalize (str, len, G_NORMALIZE_NFKC);
 }
 
+#include <stdio.h>
 /**
  * stringprep_ucs4_nfkc_normalize:
  * @str: a Unicode string.
@@ -1117,6 +1118,9 @@ stringprep_ucs4_nfkc_normalize (const uint32_t * str, ssize_t len)
   uint32_t *result_wc;
 
   p = stringprep_ucs4_to_utf8 (str, len, 0, 0);
+  if (!p)
+    return NULL;
+
   result_wc = _g_utf8_normalize_wc (p, -1, G_NORMALIZE_NFKC);
   free (p);
 
