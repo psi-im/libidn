7c02f466ed3375ab2774f2baa01b1c154cdd1861
Tim Rühsen <tim.ruehsen@gmx.de>
2017-06-30 13:03:35 +0200

    Fix NULL ptr access in nfkc.c/g_utf8_normalize()

--- lib/nfkc.c
+++ lib/nfkc.c
@@ -937,9 +937,11 @@ static gchar *
 g_utf8_normalize (const gchar * str, gssize len, GNormalizeMode mode)
 {
   gunichar *result_wc = _g_utf8_normalize_wc (str, len, mode);
-  gchar *result;
+  gchar *result = NULL;
 
-  result = g_ucs4_to_utf8 (result_wc, -1, NULL, NULL);
+  if (result_wc)
+    result = g_ucs4_to_utf8 (result_wc, -1, NULL, NULL);
+
   g_free (result_wc);
 
   return result;
